b4c709ecd14017ec22423b98070d794f6d466e3a
Kolten Robison <mosquitobird11@gmail.com>
2016-07-13 00:03:03 -0600

    EOD - Seeder list for nes is huge now

--- database/seeds/GamesSeeder.php
+++ database/seeds/GamesSeeder.php
@@ -35,13 +35,12 @@ class GamesSeeder extends Seeder
 
 
         //NES INSERTS (SMB3 As Data Test)
-        DB::table('games')->insert([
+        $smb3 = DB::table('games')->insertGetId(
         	[
-	        	'id' => 3,
 	            'name' => 'Super Mario Bros. 3',
 	            'type_id' => 1
-        	],
-        ]);
+        	]
+        );
 
         //NES INSERTS (SMB3 As Data Test)
         DB::table('neslockers')->insert([
@@ -60,7 +59,7 @@ class GamesSeeder extends Seeder
         DB::table('nes_basics')->insert([
         	[
         		'id' => 1,
-        		'game_id' => 3,
+        		'game_id' => $smb3,
                 'short' =>  "The third installment of the Super Mario Bros. Series brings new powerups, dangerous enemies, and exciting new worlds to explore!"
         	]
         ]);
@@ -69,21 +68,21 @@ class GamesSeeder extends Seeder
         DB::table('nes_achievements')->insert([
         	[
 	        	'id' => 1,
-	        	'game_id' => 3,
+	        	'game_id' => $smb3,
 	        	'name' => 'Conqueror - Grass Land',
 	        	'description' => 'Complete World 1 by defeating the airship boss.',
 	        	'neslocker_id' => 1
         	],
         	[
 	        	'id' => 2,
-	        	'game_id' => 3,
+	        	'game_id' => $smb3,
 	        	'name' => 'Conqueror - Desert Land',
 	        	'description' => 'Complete World 2 by defeating the airship boss.',
 	        	'neslocker_id' => 2
         	],
         	[
 	        	'id' => 3,
-	        	'game_id' => 3,
+	        	'game_id' => $smb3,
 	        	'name' => 'Conqueror - Water Land',
 	        	'description' => 'Complete World 3 by defeating the airship boss.',
 	        	'neslocker_id' => 3
@@ -94,14 +93,14 @@ class GamesSeeder extends Seeder
         DB::table('nes_cheats')->insert([
         	[
         		'id' => 1,
-        		'game_id' => 3,
+        		'game_id' => $smb3,
         		'name' => 'Raise power meter standing still',
         		'value' => 'AANZKLLA',
         		'description' => 'You must be Raccoon Mario for this code, which allows you to fly from a standing start'
         	],
         	[
         		'id' => 2,
-        		'game_id' => 3,
+        		'game_id' => $smb3,
         		'name' => 'Start on World 2',
         		'value' => 'PEUZUGAA',
         		'description' => NULL
@@ -111,14 +110,14 @@ class GamesSeeder extends Seeder
         //NES INSERTS (SMB3 As Data Test)
         DB::table('nes_hashes')->insert([
             [
-                'game_id' => 3,
+                'game_id' => $smb3,
                 'hash_type' => 'sha1',
                 'hash' => 'a03e7e526e79df222e048ae22214bca2bc49c449',
                 'variation_name' => '(USA)',
                 'description' => 'The original North american release of smb3'
             ],
             [
-                'game_id' => 3,
+                'game_id' => $smb3,
                 'hash_type' => 'sha1',
                 'hash' => '6bd518e85eb46a4252af07910f61036e84b020d1',
                 'variation_name' => '(USA) Revision A',
@@ -129,7 +128,7 @@ class GamesSeeder extends Seeder
         //NES INSERTS (SMB3 As Data Test)
         DB::table('nes_specs')->insert([
             [
-                'game_id' => 3,
+                'game_id' => $smb3,
                 'mapper_number' => 4,
                 'mapper_name' => 'Nintendo MMC3',
                 'prg' => '16 x 16kB',
@@ -141,21 +140,21 @@ class GamesSeeder extends Seeder
         //NES INSERTS (SMB3 As Data Test)
         DB::table('nes_releases')->insert([
             [
-                'game_id' => 3,
+                'game_id' => $smb3,
                 'year' => 1988,
                 'month' => 10,
                 'day' => 23,
                 'region' => 'Japan'
             ],
             [
-                'game_id' => 3,
+                'game_id' => $smb3,
                 'year' => 1990,
                 'month' => 2,
                 'day' => 9,
                 'region' => 'North America'
             ],
             [
-                'game_id' => 3,
+                'game_id' => $smb3,
                 'year' => 1991,
                 'month' => 8,
                 'day' => 29,
