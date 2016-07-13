d5f61e7ed2c039e05b9a46ed7d3d161199b09332
krobison <krobison@mines.edu>
2016-07-13 10:05:07 -0600

    Fixed variable height spacing for games list

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -11,7 +11,7 @@
     <h2 class="listhead">NES</h2>
     <div class="row">
     @foreach ($nes_games as $nes_game)
-    <div class="col-sm-6 col-md-3">
+    <div class="col-sm-6 col-md-3 gameCard">
 		<a href="{{url('game/'.$nes_game->id)}}">
 		<div class="thumbnail">
 			<img src="{{$nes_game->getDisplayImage('Front')}}" width=250/>
